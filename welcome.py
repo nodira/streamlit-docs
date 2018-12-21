00dc8550ccd1c6fdff3451bc8cf5248b386a325b
nodira <nodira@streamlit.io>
2018-12-20 20:50:47 -0800

    Add in tutorial from website and streamlit help content

---
+++ welcome.py
@@ -0,0 +1,16 @@
+#WELCOME_LINK
+import streamlit as st
+
+st.title('Streamlit Documentation')
+
+st.write("""
+Here are some links to get you started with Streamlit, your new secret weapon for data science and machine learning.
+Enjoy!
+- [Getting Started with Streamlit](INSTALLATION_LINK)
+- [Your First Streamlit Report](FIRST_REPORT_LINK)
+- [How to Run Streamlit Remotely] (REMOTE_OPERATION_LINK)
+- [Quick Reference](QUICK_REFERENCE_LINK)
+- [Lots of example reports!](EXAMPLES_LINK)
+
+*Suggestions for improving these docs? Please let [us](mailto:hello@streamlit.io) know!*
+""")
